9a71c3a4018f00425dc25d9f004bc72879b17ba7
Safiyeshka <koroglu1.s.i.22@gmail.com>
2023-12-05 08:45:39 +0300

    4.2

--- back/butterfly/routes/index.js
+++ back/butterfly/routes/index.js
@@ -6,4 +6,45 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+
+/*Caligo Memnon*/
+
+router.get('/caligo_memnon', function(req, res, next) {
+  res.send("<h1>Caligo Memnon</h1>")
+  });
+  
+/*Cethosia Cyane*/
+
+router.get('/cethosia_cyane', function(req, res, next) {
+  res.send("<h1>Cethosia Cyane</h1>")
+  });
+  
+/*Doleschallia Bisaltide*/
+
+router.get('/doleschallia_bisaltide', function(req, res, next) {
+  res.send("<h1>Doleschallia Bisaltide</h1>")
+  }); 
+  
+  
+/*Greta Oto*/
+
+router.get('/greta_oto', function(req, res, next) {
+  res.send("<h1>Greta Oto</h1>")
+  });   
+  
+/*Papilio Memnon*/
+
+router.get('/papilio_memnon', function(req, res, next) {
+  res.send("<h1>Papilio Memnon</h1>")
+  });   
+  
+
+/*Vindula Dejone*/
+
+router.get('/vindula_dejone', function(req, res, next) {
+  res.send("<h1>Vindula Dejone</h1>")
+  });   
+
+
+  
+  module.exports = router;
