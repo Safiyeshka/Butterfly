03903ff7a64c0bfc07532de26a7116d0cf3d0eac
Safiyeshka <koroglu1.s.i.22@gmail.com>
2024-01-03 20:28:26 +0300

    8.2

--- back/butterfly/app.js
+++ back/butterfly/app.js
@@ -10,6 +10,8 @@ mongoose.connect('mongodb://localhost/butterflies')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var butterflies = require('./routes/butterflies');
+
 
 var app = express();
 
@@ -26,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/butterflies', butterflies);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
