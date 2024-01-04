addcec2bb4a2edabfde0f9e92757ecbf67c526c4
Safiyeshka <koroglu1.s.i.22@gmail.com>
2024-01-05 00:02:24 +0300

    8.3

--- back/butterfly/routes/butterflies.js
+++ back/butterfly/routes/butterflies.js
@@ -1,33 +1,49 @@
 var express = require('express');
 var router = express.Router();
-var async = require("async");
+// var async = require("async");
 var Butterfly = require("../models/butterfly").Butterfly
 
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с butterflies');
-});
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('Новый маршрутизатор, для маршрутов, начинающихся с butterflies');
+// });
 
     
-/* Страница героев */
-router.get("/:nick", async (req, res, next) => {
+// /* Страница героев */
+// router.get("/:nick", async (req, res, next) => {
+//   try {
+//     const butterfly = await Butterfly.findOne({ nick: req.params.nick });
+//     console.log(butterfly);
+//     if (!butterfly) {
+//       throw new Error("");
+//     }
+//     res.render('butterfly', {
+//       title: butterfly.title,
+//       picture: butterfly.avatar,
+//       desc: butterfly.desc
+//     });
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+router.get('/:nick', async function(req, res, next) {
   try {
-    const butterfly = await Butterfly.findOne({ nick: req.params.nick });
-    console.log(butterfly);
+    const [butterfly, butterflies] = await Promise.all([
+      Butterfly.findOne({ nick: req.params.nick }),
+      Butterfly.find({}, { _id: 0, title: 1, nick: 1 })
+    ]);
+
     if (!butterfly) {
       throw new Error("Нет такой бабочки");
     }
-    res.render('butterfly', {
-      title: butterfly.title,
-      picture: butterfly.avatar,
-      desc: butterfly.desc
-    });
+    
+    renderSpace(res, butterfly.title, butterfly.avatar, butterfly.desc, butterflies);
   } catch (err) {
     next(err);
   }
 });
-  
 
 
 module.exports = router;
